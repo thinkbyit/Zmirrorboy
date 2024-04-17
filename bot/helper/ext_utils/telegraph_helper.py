8dc633c87d2aae07ef8092c72fbf5825d4e0a613
thinkbyit <princejaiswal9966@gmail.com>
2024-04-17 18:30:45 +0530

    template change

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -102,5 +102,5 @@ class TelegraphHelper:
 
 
 telegraph = TelegraphHelper(
-    'Z-Mirror', 'https://github.com/Dawn-India/Z-Mirror')
+    'Z-Mirror', 'https://t.me/thinkbyit/55')
 bot_loop.run_until_complete(telegraph.create_account())
